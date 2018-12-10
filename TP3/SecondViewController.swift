c32aae0da923d5a7192ab69f4b5555e14d7f57a0
HAAS Romain <hr812106@s126p15.local>
2018-12-10 08:18:56 +0100

    Initial Commit

---
+++ TP3/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TP3
+//
+//  Created by HAAS Romain on 10/12/2018.
+//  Copyright © 2018 HAAS Romain. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
